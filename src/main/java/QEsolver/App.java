0ca7b6140042ddc53b06414ffd0ed4e18f6cb281
Olha Kulyk <Olha_Kulyk@epam.com>
2017-05-15 14:31:43 +0300

    Refactoring existing project

---
+++ src/main/java/QEsolver/App.java
@@ -0,0 +1,13 @@
+package QEsolver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/QEsolver/Solving.java
@@ -0,0 +1,27 @@
+package QEsolver;
+
+public class Solving {
+	
+	public double[] solvingEquatution (double a, double b, double c, double D) throws DivExeption{
+		
+		double[] resultArray = new double[2];
+		double x1;
+		double x2;
+		
+		if (a == 0.0){
+			throw new DivExeption();}
+		
+		x1 = (-b + Math.sqrt(D))/(2*a);
+		x2 = (-b - Math.sqrt(D))/(2*a);
+		resultArray[0] = x1;
+		resultArray[1] = x2;
+		return resultArray;}
+	
+	public double solvingEquatution (double a, double b)  throws DivExeption{
+		
+		if (a == 0.0){
+			throw new DivExeption();}
+		
+		double x1 = -b/(2*a);
+		return x1;}
+}
